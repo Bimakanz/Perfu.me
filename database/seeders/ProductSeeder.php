28e6ab34e0bcd070caff147765573d44a720d965
Bimakanz <bimasenayusuf1.xpro@gmail.com>
2026-09-12 17:29:52 +0700

    Fix Hosting

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,12 +4,15 @@ namespace Database\Seeders;
 
 use App\Models\Product;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class ProductSeeder extends Seeder
 {
     public function run(): void
     {
+        Schema::disableForeignKeyConstraints();
         Product::truncate();
+        Schema::enableForeignKeyConstraints();
 
         $products = [
             [
